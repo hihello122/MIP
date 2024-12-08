84c4709e278e37d3910568c215aa1d15433352a0
hihello122 <tprmatpxkr@gmail.com>
2024-12-08 10:30:35 +0900

    trainer fix

--- trainers/mip.py
+++ trainers/mip.py
@@ -21,8 +21,7 @@ _tokenizer = _Tokenizer()
 def load_clip_to_cpu(cfg):
     backbone_name = cfg.MODEL.BACKBONE.NAME
     url = clip._MODELS[backbone_name]
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model_path = clip._download(url,device)
+    model_path = clip._download(url,osp.expanduser("~/.cache/clip"))
 
 
     try:
